f4c8f9f97ead38f47c70d6af8e22a1749aa9921e
Cyril5169 <516974551@qq.com>
2020-02-23 13:53:48 +0800

    登录页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import store from '.././store'
 import Router from 'vue-router'
+import Login from '@/components/login'
 import Main from '@/components/main'
 import MainOld from '@/components/mainOld'
 import Test from '@/components/test'
@@ -14,12 +15,17 @@ Vue.use(Router)
 const router = new Router({
   routes: [
     {
+      path: '/login',
+      component: Login,
+      name: 'login'
+    },
+    {
       path: '*',
-      redirect: '/main'
+      redirect: '/login'
     },
     {
       path: '',
-      redirect: '/main'
+      redirect: '/login'
     },
     {
       path: '/main',
@@ -58,23 +64,23 @@ Router.prototype.push = function push(location) {
 
 //路由守卫
 router.beforeEach((to, from, next) => {
-  //其他具体需求开发时再加。如只允许一个账号登录等。
-  // if (from.path == '/' && to.meta.returnMain) {//如果是刷新判断是否需要回到主界面
-  //   if (to.name == 'login' || to.name == 'main') {
-  //     next()
-  //   }
-  //   else {
-  //     next('/main');
-  //   }
+  // if (!Cookies.get('cid') && !Cookies.get('customerType') && to.name != 'login') {//判断用户信息，不合法返回登陆界面
+  //   next('/login')
+  // } else {
+    if (from.path == '/') {
+      store.commit('navTabs/emptyBreadCrumb');//到主页面后清空导航
+      if (to.name == 'login' || to.name == 'main') {
+        next()
+      }
+      else {
+        next('/main');
+      }
+    }
+    else {//不是刷新
+      next();
+      history.pushState(null, null, location.href);//禁止后退，搭配APP.VUE里面的mounted
+    }
   // }
-  if (from.path == '/' && to.path !='/main') {//所有刷新回到主界面
-    store.commit('navTabs/emptyBreadCrumb');//到主页面后清空导航
-    next('/main');
-  }
-  else {//不是刷新
-    next();
-    history.pushState(null, null, location.href);//禁止后退，搭配APP.VUE里面的mounted
-  }
 })
 
 export default router;
