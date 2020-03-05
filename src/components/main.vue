c06dafacf68db80d858ddc417f2d6a2a06292456
ZhuSongPing <53893165+ZhuSongPing@users.noreply.github.com>
2020-03-05 23:25:14 +0800

    物料信息提交

--- src/components/main.vue
+++ src/components/main.vue
@@ -41,6 +41,7 @@
                 <el-menu-item index="dept" route="/dept">部门</el-menu-item>
                 <el-menu-item index="emp" route="/emp">人员</el-menu-item>
                 <el-menu-item index="project_classification" route="/project_classification">项目分类</el-menu-item>
+                <el-menu-item index="item" route="/item">物料信息</el-menu-item>
               </el-menu-item-group>
             </el-submenu>
             <!-- 权限树加载 -->
