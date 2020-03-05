77de9fe7c335701d0f1be56a189ee6364459ba4c
zhangtao1995 <119633951@qq.com>
2020-03-05 21:09:52 +0800

    部门人员

--- src/components/hr/tabComponentsForDept/tabChild2.vue
+++ src/components/hr/tabComponentsForDept/tabChild2.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="child1">
-        这是第二个tab-部门人员信息
+        这是第二个tab-部门班次
     </div>
 </template>
 
 <script>
     export default {
-      name: 'child1',
+      name: 'child2',
       mounted(){
           console.log("部门人员信息")
       }
